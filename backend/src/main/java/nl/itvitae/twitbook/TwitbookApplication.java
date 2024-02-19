928087c3d5bdd2f9812cc41ea62797453d5475ef
Melle Gubbels <melle.gubbels@itvitaelearning.nl>
2024-02-19 10:21:19 +0100

    Initializing

---
+++ backend/src/main/java/nl/itvitae/twitbook/TwitbookApplication.java
@@ -0,0 +1,13 @@
+package nl.itvitae.twitbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitbookApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/nl/itvitae/twitbook/TwitbookApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.itvitae.twitbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
