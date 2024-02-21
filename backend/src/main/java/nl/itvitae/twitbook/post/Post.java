9ca8615c5a3e99b24c5bea500a32c350ec2b17ec
melleg <kmm.gubbels@gmail.com>
2024-02-21 13:18:39 +0100

    Refactored imports

--- backend/src/main/java/nl/itvitae/twitbook/post/Post.java
+++ backend/src/main/java/nl/itvitae/twitbook/post/Post.java
@@ -1,6 +1,10 @@
 package nl.itvitae.twitbook.post;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
 
 import java.time.LocalDateTime;
 
