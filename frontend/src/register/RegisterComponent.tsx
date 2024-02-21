c23a32d0b41e8f4882f9284566bba34ac4de40aa
NolKeereweer <nol.keereweer202@gmail.com>
2024-02-21 13:34:48 +0100

    Post-merge fix
    Fixed an error with the naming of a file.

--- frontend/src/register/RegisterComponent.tsx
+++ frontend/src/register/RegisterComponent.tsx
@@ -1,39 +1,39 @@
 import { useState } from "react";
 
-const RegisterComponent = () =>  {
+const RegisterComponent = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [checkPassword, setCheckPassword] = useState("");
 
   return (
     <div className=" text-center">
-      <form 
+      <form
         onSubmit={(e) => {
           e.preventDefault();
         }}
       >
         <label className="px-2">Enter username: </label>
-        <input 
-        type="text"
-        placeholder="username"
-        onChange={(e) => setUsername(e.target.value)}
+        <input
+          type="text"
+          placeholder="username"
+          onChange={(e) => setUsername(e.target.value)}
         />
         <label className="px-2">Enter password: </label>
-        <input 
-        type="text"
-        placeholder="password"
-        onChange={(e) => setCheckPassword(e.target.value)}
+        <input
+          type="text"
+          placeholder="password"
+          onChange={(e) => setCheckPassword(e.target.value)}
         />
         <label className="px-2">Re-enter password: </label>
-        <input 
-        type="text"
-        placeholder="re-enter password"
-        onChange={(e) => setPassword(e.target.value)}
+        <input
+          type="text"
+          placeholder="re-enter password"
+          onChange={(e) => setPassword(e.target.value)}
         />
         <button type="submit">Register</button>
       </form>
     </div>
   );
-}
+};
 
 export default RegisterComponent;
