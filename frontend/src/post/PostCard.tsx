c978842d9ba6fc3877a54507d1bf5cec3683526f
RaafiKuijper <raafi.kuijper@itvitaelearning.nl>
2024-02-23 12:43:02 +0100

    Add frontend response on delete

--- frontend/src/post/PostCard.tsx
+++ frontend/src/post/PostCard.tsx
@@ -2,46 +2,62 @@ import { Link } from "react-router-dom";
 import Post from "./post";
 import { format } from "date-fns";
 import { deletePost } from "./post-service";
+import { useState } from "react";
 
 interface PostCardProps {
   post: Post;
 }
 
 const PostCard: React.FC<PostCardProps> = ({ post }) => {
+  const [deleted, setDeleted] = useState<boolean>(false);
+
   const handleDelete = () => {
     deletePost(post.id);
+    setDeleted(true);
   };
   return (
-    <div className="p-4 glass rounded-lg flex flex-wrap items-start gap-2">
-      <Link to={`/profile/${post.username}`}>
-        <img
-          className="w-14 rounded-full aspect-square"
-          src="https://picsum.photos/50"
-        ></img>
-      </Link>
-      <div className="flex flex-col">
-        <div className="flex flex-wrap gap-2 items-center">
-          <Link to={`/profile/${post.username}`}>
-            <h4>@{post.username}</h4>
-          </Link>
-          <span className="text-light">
-            - {format(post.postedDate, "dd MMMM yyyy")}
-          </span>
-        </div>
-        <p>{post.content}</p>
-        <div className="flex flex-wrap gap-2 mt-1">
-          <button className="btn-icon w-16 text-left" type="button">
-            👍1k
-          </button>
-          <button className="btn-icon w-16 text-left" type="button">
-            🔁1k
-          </button>
-          <button className="btn-icon text-left" type="button"             onClick={handleDelete}>
-            Delete Post
-          </button>
-        </div>
+    <>
+      <div className="p-4 glass rounded-lg flex flex-wrap items-start gap-2">
+        {deleted ? (
+          <p>Post deleted</p>
+        ) : (
+          <>
+            <Link to={`/profile/${post.username}`}>
+              <img
+                className="w-14 rounded-full aspect-square"
+                src="https://picsum.photos/50"
+              ></img>
+            </Link>
+            <div className="flex flex-col">
+              <div className="flex flex-wrap gap-2 items-center">
+                <Link to={`/profile/${post.username}`}>
+                  <h4>@{post.username}</h4>
+                </Link>
+                <span className="text-light">
+                  - {format(post.postedDate, "dd MMMM yyyy")}
+                </span>
+              </div>
+              <p>{post.content}</p>
+              <div className="flex flex-wrap gap-2 mt-1">
+                <button className="btn-icon w-16 text-left" type="button">
+                  👍1k
+                </button>
+                <button className="btn-icon w-16 text-left" type="button">
+                  🔁1k
+                </button>
+                <button
+                  className="btn-icon text-left"
+                  type="button"
+                  onClick={handleDelete}
+                >
+                  Delete Post
+                </button>
+              </div>
+            </div>
+          </>
+        )}
       </div>
-    </div>
+    </>
   );
 };
 
