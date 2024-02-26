a50363b5a6b704e2c113151782382b6044a2d395
melleg <kmm.gubbels@gmail.com>
2024-02-26 13:10:41 +0100

    Added repost visibility frontend

--- frontend/src/post/PostCard.tsx
+++ frontend/src/post/PostCard.tsx
@@ -16,15 +16,34 @@ const PostCard: React.FC<PostCardProps> = ({ post }) => {
         ></img>
       </Link>
       <div className="flex flex-col">
-        <div className="flex flex-wrap gap-2 items-center">
-          <Link to={`/profile/${post.username}`}>
-            <h4>@{post.username}</h4>
+        <div className="flex flex-wrap gap-1 items-center">
+          <Link to={`/profile/${post.username}`} className="h4">
+            @{post.username}
           </Link>
+          {post.linkedPost && (<span className="h4 text-light">replied</span>)}
           <span className="text-light">
             - {format(post.postedDate, "dd MMMM yyyy")}
           </span>
         </div>
+        
+        {/* Content */}
         <p>{post.content}</p>
+
+        {/* Reposts */}
+        {post.linkedPost && (
+        <div className="rounded-lg border-2 border-gray-500 p-2 mt-1">
+          <img
+            className="w-6 mr-1 inline-block rounded-full aspect-square"
+            src="https://picsum.photos/50">
+          </img>
+          <Link to={`/profile/${post.linkedPost.username}`} className="h4 mr-1" >
+            @{post.linkedPost.username}
+          </Link>
+          <span className="text-light">{format(post.postedDate, "dd MMMM yyyy")}</span>
+          <p>"{post.linkedPost.content}"</p>
+        </div>)}
+
+        {/* Bottom buttons */}
         <div className="flex flex-wrap gap-2 mt-1">
           <button className="btn-icon w-16 text-left" type="button">
             👍1k
