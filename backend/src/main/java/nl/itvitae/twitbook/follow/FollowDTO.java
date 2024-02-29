bbb5e01095ea76dffc013d6f901212f2a0c8d78a
RaafiKuijper <raafi.kuijper@itvitaelearning.nl>
2024-02-29 10:58:34 +0100

    Remove unused imports

--- backend/src/main/java/nl/itvitae/twitbook/follow/FollowDTO.java
+++ backend/src/main/java/nl/itvitae/twitbook/follow/FollowDTO.java
@@ -1,9 +1,5 @@
 package nl.itvitae.twitbook.follow;
 
-import java.time.LocalDateTime;
-import java.util.UUID;
-import nl.itvitae.twitbook.post.Post;
-
 public record FollowDTO(Long id, String followerUsername, String followingUsername) {
   public FollowDTO(Follow follow) {
     this(follow.getId(), follow.getFollower().getUsername(), follow.getFollowing().getUsername());
