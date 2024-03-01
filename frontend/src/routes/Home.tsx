56b1a4287b5d5affecf4ebad383325c71bbeef87
RaafiKuijper <raafi.kuijper@itvitaelearning.nl>
2024-03-01 13:55:59 +0100

    Add toggle to view all posts or followed only

--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -7,14 +7,24 @@ import { useGlobalContext } from "../auth/GlobalContext";
 function Home() {
   const [refresh, setRefresh] = useState<number>(0);
   const { loggedIn } = useGlobalContext();
+  const [viewAll, setViewAll] = useState<boolean>(false);
+
+  const handleToggle = () => {
+    setViewAll(!viewAll);
+    setRefresh(refresh + 1);
+  };
 
   return (
     <>
-      {loggedIn ? (
+      {loggedIn && (
         <>
-          <CreatePostComponent onSubmit={() => setRefresh(refresh + 1)} />
-          <Feed getFunction={getPostsByFollowing()} refresh={refresh} />
+        <CreatePostComponent onSubmit={() => setRefresh(refresh + 1)} />
+        <button className="btn-action my-4" onClick={() => handleToggle()}>{viewAll ? "View all posts" : "View followers only"}</button>
         </>
+      )}
+
+      {viewAll ? (
+        <Feed getFunction={getPostsByFollowing()} refresh={refresh} />
       ) : (
         <Feed getFunction={getPosts()} refresh={refresh} />
       )}
