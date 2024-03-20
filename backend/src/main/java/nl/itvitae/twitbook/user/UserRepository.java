babc52886698846e9c35c11e434feddd8a09b56e
melleg <kmm.gubbels@gmail.com>
2024-03-20 12:01:22 +0100

    Added getAll pageable method

--- backend/src/main/java/nl/itvitae/twitbook/user/UserRepository.java
+++ backend/src/main/java/nl/itvitae/twitbook/user/UserRepository.java
@@ -2,11 +2,15 @@ package nl.itvitae.twitbook.user;
 
 import java.util.Optional;
 import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-
   Optional<User> findByUsernameIgnoreCase(String username);
+
+  Page<User> getAll(Pageable pageable);
 
   boolean existsByUsernameIgnoreCase(String username);
 }
