a9e1a083c14e83de3111eaeb9f0cd5e375b41bb4
RaafiKuijper <raafi.kuijper@itvitaelearning.nl>
2024-03-20 12:37:54 +0100

    small rename

--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -44,7 +44,7 @@ function Home() {
               className="btn-action my-4"
               onClick={() => showFollowingOnly(false)}
             >
-              View followers only
+              View personal feed
             </button>
           </div>
           <CreatePostComponent />
