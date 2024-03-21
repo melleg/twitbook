10ef85baf5ef87631559ea96b78122fc11762138
NolKeereweer <nol.keereweer202@gmail.com>
2024-03-21 11:36:01 +0100

    Added derived queries and endpoints
    Created backend support to get paginated lists of Followers/following.

--- backend/src/main/java/nl/itvitae/twitbook/user/UserController.java
+++ backend/src/main/java/nl/itvitae/twitbook/user/UserController.java
@@ -6,6 +6,11 @@ import java.util.Optional;
 import lombok.AllArgsConstructor;
 
 import nl.itvitae.twitbook.follow.FollowRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -24,6 +29,15 @@ public class UserController {
   private final UserRepository userRepository;
   private final FollowRepository followRepository;
 
+  private static final int PAGE_SIZE = 4;
+
+  private PageRequest getPageable(Pageable pageable) {
+    return PageRequest.of(
+        pageable.getPageNumber(),
+        Math.min(pageable.getPageSize(), PAGE_SIZE),
+        Sort.by(Direction.ASC, "displayName"));
+  }
+
   @GetMapping
   public List<UserDTO> getAll() {
     return userRepository.findAll().stream().map(UserDTO::new).toList();
@@ -31,19 +45,55 @@ public class UserController {
 
   @GetMapping("by-username/{username}")
   public ResponseEntity<?> findByUsername(@PathVariable String username,
-      @AuthenticationPrincipal User user) {
+      @AuthenticationPrincipal User callingUser) {
     Optional<User> targetUser = userRepository.findByUsernameIgnoreCase(username);
 
     if (targetUser.isEmpty()) {
       return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 
-    if (user == null) {
+    if (callingUser == null) {
       return new ResponseEntity<>(new UserDTO(targetUser.get()), HttpStatus.OK);
     }
     return new ResponseEntity<>(new UserDTO(targetUser.get(),
-        followRepository.existsFollowByFollowerIdAndFollowingId(user.getId(),
+        followRepository.existsFollowByFollowerIdAndFollowingId(callingUser.getId(),
             targetUser.get().getId())), HttpStatus.OK);
+  }
+
+  @GetMapping("followers-by-username/{username}")
+  public ResponseEntity<?> findFollowersByUsername(@PathVariable String username,
+      @AuthenticationPrincipal User callingUser, Pageable pageable) {
+
+    Optional<User> targetUser = userRepository.findByUsernameIgnoreCase(username);
+
+    if (targetUser.isEmpty()) {
+      return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
+    }
+
+    Page<User> followersPage = userRepository.findByFollowing_Following_UsernameIgnoreCase(targetUser.get().getUsername(),
+        getPageable(pageable));
+
+    Page<UserDTO> followersDTOPage = followersPage.map(UserDTO::new);
+
+    return new ResponseEntity<>(followersDTOPage, HttpStatus.OK);
+  }
+
+  @GetMapping("following-by-username/{username}")
+  public ResponseEntity<?> findFollowingByUsername(@PathVariable String username,
+      @AuthenticationPrincipal User callingUser, Pageable pageable) {
+
+    Optional<User> targetUser = userRepository.findByUsernameIgnoreCase(username);
+
+    if (targetUser.isEmpty()) {
+      return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
+    }
+
+    Page<User> followingPage = userRepository.findByFollowers_Follower_UsernameIgnoreCase(targetUser.get().getUsername(),
+        getPageable(pageable));
+
+    Page<UserDTO> followingDTOPage = followingPage.map(UserDTO::new);
+
+    return new ResponseEntity<>(followingDTOPage, HttpStatus.OK);
   }
 
   private record UserUsernameBioOnly(String displayName, String bio) {
--- backend/src/main/java/nl/itvitae/twitbook/user/UserRepository.java
+++ backend/src/main/java/nl/itvitae/twitbook/user/UserRepository.java
@@ -1,12 +1,19 @@
 package nl.itvitae.twitbook.user;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
   Optional<User> findByUsernameIgnoreCase(String username);
+
+  Page<User> findByFollowing_Following_UsernameIgnoreCase(String username, Pageable pageable);
+
+  Page<User> findByFollowers_Follower_UsernameIgnoreCase(String username, Pageable pageable);
 
   boolean existsByUsernameIgnoreCase(String username);
 }
