ba4c38ba60d6928b33de11241b4b405304013c55
NolKeereweer <nol.keereweer202@gmail.com>
2024-03-21 11:36:32 +0100

    Added front-end calls to the new endpoints

--- frontend/src/user/user-service.ts
+++ frontend/src/user/user-service.ts
@@ -12,6 +12,14 @@ export const getUserByUsername = async (username: string) => {
   return (await api.get<User>(`${uri}/by-username/${username}`)).data;
 };
 
+export const getFollowers = async (username: string) => {
+  return (await api.get<User>(`${uri}/followers-by-username/${username}`)).data;
+};
+
+export const getFollowing = async (username: string) => {
+  return (await api.get<User>(`${uri}/following-by-username/${username}`)).data;
+};
+
 export const followUser = async (followUsername: string) => {
   return (await api.post(`follows/user/${followUsername}`));
 };
